5e8a3bd8e2991257b1fbb283f4fb7b15962f68f6
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-22 16:19:47 +0100

    Create class SimpleBowling

---
+++ Bowling/Bowling/SimpleBowling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    public class SimpleBowling : IBowling
+    {
+        public void CountScore(ref BowlingScore score)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
