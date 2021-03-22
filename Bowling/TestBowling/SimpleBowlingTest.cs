84d32077ddb02448af02ad91966d961bdaa585c1
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-22 19:19:21 +0100

    Add OneSpareBonusScore Test

--- Bowling/TestBowling/SimpleBowlingTest.cs
+++ Bowling/TestBowling/SimpleBowlingTest.cs
@@ -44,6 +44,37 @@ namespace TestBowling
 
             Assert.AreEqual(69, finalScore);
         }
-       
+
+        /// <summary>
+        /// Final score with only one spare bonus
+        /// </summary>
+        [TestMethod]
+        public void OneSpareBonusScore()
+        {
+            var points = new int[] {
+                5,5, // Round 1
+                3,4, // Round 2
+                7,1, // Round 3
+                1,1, // Round 4
+                2,5, // Round 5
+                9,0, // Round 6
+                2,3, // Round 7
+                4,4, // Round 8
+                3,6, // Round 9
+                1,4, // Round 10
+                -1,-1 // No additional throws
+            };
+
+            BowlingScore score = new BowlingScore(name, points);
+
+            IBowling bowling = new SimpleBowling();
+
+            bowling.CountScore(ref score);
+
+            int finalScore = score.Score;
+
+            Assert.AreEqual(77, finalScore);
+        }
+
     }
 }
