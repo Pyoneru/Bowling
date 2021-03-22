79d4f84403c0adb631327b840c7aa02b074d30ce
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-22 20:43:03 +0100

    Create HTMLOutput class

---
+++ Bowling/Bowling/HTMLOutput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    public class HTMLOutput : IOutput
+    {
+        public int CreateOutput(ref ICollection<BowlingScore> bowlings, string output)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetResultMessage(int result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
