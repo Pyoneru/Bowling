1cc96f4528b67c1bff6fec1ecff7a39ef2d76e8b
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-23 22:20:15 +0100

    Add ICommand interface

---
+++ Bowling/BowlingConsole/Command/ICommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingConsole.Command
+{
+    public interface ICommand: ICloneable
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// Full flag name(for example --print)
+        /// </summary>
+        public string FullFlag { get; set; }
+
+        /// <summary>
+        /// Short flag name(for example -p)
+        /// </summary>
+        public string ShortFlag { get; set; }
+        /// <summary>
+        /// Descrption is used to print help
+        /// </summary>
+        public string Description { get; set; }
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Provided needed data to execute command
+        /// </summary>
+        /// <param name="data">data</param>
+        public void SetData(params object[] data);
+
+        /// <summary>
+        /// Execute command
+        /// </summary>
+        public void Execute();
+
+        #endregion Methods
+    }
+}
