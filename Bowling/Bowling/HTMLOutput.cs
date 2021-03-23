5bb4b307ecce44b63f65f609e40a9807af0a117d
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-23 16:54:11 +0100

    CodeReview HTMLOutput

--- Bowling/Bowling/HTMLOutput.cs
+++ Bowling/Bowling/HTMLOutput.cs
@@ -9,13 +9,30 @@ using RazorEngine.Templating;
 
 namespace Bowling
 {
+    /// <summary>
+    /// Implementation of IOutput.
+    /// Create output in html format.
+    /// Implementation using RazorEngine library
+    /// </summary>
     public class HTMLOutput : IOutput
     {
+        /// <summary>
+        /// Key needed by RazorEngine
+        /// </summary>
         protected const string KEY = "html_bowling";
+
+        /// <summary>
+        /// By default is used DEFAULT_TEMPLATE_PATH to finding template file it you will not provide custom path.
+        /// </summary>
         protected const string DEFAULT_TEMPLATE_PATH = "template.cshtml";
+
         public dynamic Output { get; set; }
+
         public bool CreateFileOutput { get; set; }
 
+        /// <summary>
+        /// Path to template
+        /// </summary>
         public string TemplatePath { get; set; }
 
         public HTMLOutput(string templatePath)
@@ -23,13 +40,22 @@ namespace Bowling
             templatePath = TemplatePath;
         }
 
+        /// <summary>
+        /// Constructor without use default template path
+        /// </summary>
         public HTMLOutput(): this(DEFAULT_TEMPLATE_PATH) { }
 
+
+        /// <summary>
+        /// Implementation method.
+        /// Create Output. Save to file if option is enabled and return Output.
+        /// </summary>
+        /// <param name="bowlings">Collection of BowlingScore</param>
+        /// <param name="output">filename to save</param>
+        /// <returns>Output</returns>
         public dynamic CreateOutput(ref ICollection<BowlingScore> bowlings, string output)
         {
-            var template = File.ReadAllText(TemplatePath);
-
-            Output = Engine.Razor.RunCompile(template, KEY, null, bowlings);
+            CreateOutputFromTemplate(ref bowlings);
 
             if (CreateFileOutput)
             {
@@ -39,11 +65,32 @@ namespace Bowling
             return Output;
         }
 
+        /// <summary>
+        /// Create output html.
+        /// Separate creating output from saving to file.
+        /// </summary>
+        /// <param name="bowlings">Collection of BowlingScore</param>
+        protected void CreateOutputFromTemplate(ref ICollection<BowlingScore> bowlings)
+        {
+            var template = File.ReadAllText(TemplatePath);
+
+            Output = Engine.Razor.RunCompile(template, KEY, null, bowlings);
+        }
+
+        /// <summary>
+        /// Invoke SaveToFileAsync
+        /// </summary>
+        /// <param name="filename">filename to save</param>
         public void SaveToFile(string filename)
         {
             _ = SaveToFileAsync(filename);
         }
 
+        /// <summary>
+        /// Asynchronous saving Output to file.
+        /// </summary>
+        /// <param name="filename">filename to save</param>
+        /// <returns>Task</returns>
         protected async Task SaveToFileAsync(string filename)
         {
             await File.WriteAllTextAsync(filename, Output);
