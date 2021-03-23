e612fcec7d233c594f306b13f766f2b223eedc6c
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-23 13:37:45 +0100

    Config HTMLOutputTest, no create output file

--- Bowling/TestBowling/HTMLOutputTest.cs
+++ Bowling/TestBowling/HTMLOutputTest.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bowling;
 
 namespace TestBowling
 {
     [TestClass]
     public class HTMLOutputTest
     {
+        private HTMLOutput output;
+
+        /// <summary>
+        /// For test, do not create output file. Output will be created only in Output propertie.
+        /// </summary>
+        public HTMLOutputTest()
+        {
+            output = new HTMLOutput();
+            output.CreateFileOutput = false;
+        }
     }
 }
