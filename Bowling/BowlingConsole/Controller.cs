765f18076f26165e9cae94694f646964e62ec84e
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-24 15:16:43 +0100

    Make Controller as abstract class -Define constructor, fields and abstract method Run()

--- Bowling/BowlingConsole/Controller.cs
+++ Bowling/BowlingConsole/Controller.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BowlingConsole.Command;
+using BowlingConsole.Util;
+using Bowling;
 
 namespace BowlingConsole
 {
-    public class Controller
+    public abstract class Controller
     {
+        protected CommandFactory factory;
+        protected string[] args;
+
+        public Controller(CommandFactory factory, string[] args)
+        {
+            this.factory = factory;
+            this.args = args;
+        }
+
+        public abstract void Run(); 
     }
 }
