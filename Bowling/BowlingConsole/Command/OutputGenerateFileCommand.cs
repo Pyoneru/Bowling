595632d9387fa7fafd427bb6df53d664ac2f0371
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-24 12:36:16 +0100

    Add OutputGenerateFileCommand class

---
+++ Bowling/BowlingConsole/Command/OutputGenerateFileCommand.cs
@@ -0,0 +1,62 @@
+﻿using Bowling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingConsole.Command
+{
+    /// <summary>
+    /// Set CreateFileOutput propertie
+    /// </summary>
+    public class OutputGenerateFileCommand : ICommand
+    {
+        public string FullFlag { get; set; }
+        public string ShortFlag { get; set; }
+        public string Description { get; set; }
+
+        protected IOutput output;
+        protected bool generate;
+
+        /// <summary>
+        /// Copy only properties. 
+        /// </summary>
+        /// <returns>Clone of instance</returns>
+        public object Clone()
+        {
+            OutputGenerateFileCommand command = new OutputGenerateFileCommand();
+            command.FullFlag = FullFlag;
+            command.ShortFlag = ShortFlag;
+            command.Description = Description;
+            return command;
+        }
+
+        /// <summary>
+        /// Set CreateFileOutput properties in IOutput instance
+        /// </summary>
+        public void Execute()
+        {
+            output.CreateFileOutput = generate;
+        }
+
+        /// <summary>
+        /// Set generate or use false by defualt
+        /// </summary>
+        /// <param name="data">IOuput and bool for setting generate</param>
+        public void SetData(params object[] data)
+        {
+            if(data.Length > 0)
+            {
+                output = (IOutput)data[0];
+
+                if (data.Length > 1)
+                {
+                    generate = (bool)data[1];
+                }
+                else
+                {
+                    generate = false;
+                }
+            }
+        }
+    }
+}
