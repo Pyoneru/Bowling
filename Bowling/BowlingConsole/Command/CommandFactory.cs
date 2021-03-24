e797550cef3bd27cf58f1f1a1396be1aab7257d8
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-24 11:25:27 +0100

    Add CommandFactory

---
+++ Bowling/BowlingConsole/Command/CommandFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingConsole.Command
+{
+    public class CommandFactory
+    {
+        protected Dictionary<string, ICommand> prototypes;
+
+        public CommandFactory()
+        {
+            prototypes = new Dictionary<string, ICommand>();
+        }
+
+        public ICommand CreateCommand(string flag)
+        {
+            return null;
+        }
+    }
+}
