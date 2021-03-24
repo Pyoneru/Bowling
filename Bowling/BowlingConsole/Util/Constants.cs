7a582c3d866eb1a0efe6b91e43fc6dd107919bfb
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-24 11:36:05 +0100

    Add constants for HelpCommand

--- Bowling/BowlingConsole/Util/Constants.cs
+++ Bowling/BowlingConsole/Util/Constants.cs
@@ -10,6 +10,9 @@ namespace BowlingConsole.Util
         public const string PRINT_COMMAND_SHORT_FLAG = "-p";
         public const string PRINT_COMMAND_DESCRIPTION = "Print output in console";
 
+        public const string HELP_COMMAND_FULL_FLAG = "--help";
+        public const string HELP_COMMAND_SHORT_FLAG = "-h";
+        public const string HELP_COMMAND_DESCRIPTION = "Use --help and command flag to print description for this command";
 
     }
 }
