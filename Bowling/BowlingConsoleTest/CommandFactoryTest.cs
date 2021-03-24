3dac5742d5f33529bece22e6d1b60d30a4877e0b
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-24 13:45:49 +0100

    Add CreateCommandIsNotNull Test

--- Bowling/BowlingConsoleTest/CommandFactoryTest.cs
+++ Bowling/BowlingConsoleTest/CommandFactoryTest.cs
@@ -15,82 +15,15 @@ namespace BowlingConsoleTest
             factory = new CommandFactory();
         }
 
+        /// <summary>
+        /// Returned command should not be null when flag is correct
+        /// </summary>
         [TestMethod]
         public void CreateCommandIsNotNull()
         {
             ICommand command = factory.CreateCommand(Constants.PRINT_COMMAND_FULL_FLAG);
 
             Assert.IsNotNull(command);
-        }
-
-        [TestMethod]
-        public void BadFlagCommandShouldReturnNull()
-        {
-            string badFlag = "badFlag";
-
-            ICommand command = factory.CreateCommand(Constants.PRINT_COMMAND_FULL_FLAG);
-
-            Assert.IsNull(command);
-        }
-
-        [TestMethod]
-        public void CreatePrintCommandFullFlag()
-        {
-            ICommand command = factory.CreateCommand(Constants.PRINT_COMMAND_FULL_FLAG);
-
-            var isPrintCommand = command is PrintCommand;
-
-            Assert.IsTrue(isPrintCommand);
-        }
-
-        [TestMethod]
-        public void CreatePrintCommandShortFlag()
-        {
-            ICommand command = factory.CreateCommand(Constants.PRINT_COMMAND_SHORT_FLAG);
-
-            var isPrintCommand = command is PrintCommand;
-
-            Assert.IsTrue(isPrintCommand);
-        }
-
-        [TestMethod]
-        public void CreateHelpCommandFullFlag()
-        {
-            ICommand command = factory.CreateCommand(Constants.HELP_COMMAND_FULL_FLAG);
-
-            var isPrintCommand = command is HelpCommand;
-
-            Assert.IsTrue(isPrintCommand);
-        }
-
-        [TestMethod]
-        public void CreateHelpCommandShortFlag()
-        {
-            ICommand command = factory.CreateCommand(Constants.HELP_COMMAND_SHORT_FLAG);
-
-            var isPrintCommand = command is HelpCommand;
-
-            Assert.IsTrue(isPrintCommand);
-        }
-
-        [TestMethod]
-        public void CreateErrorCommandFullFlag()
-        {
-            ICommand command = factory.CreateCommand(Constants.HELP_COMMAND_FULL_FLAG);
-
-            var isPrintCommand = command is HelpCommand;
-
-            Assert.IsTrue(isPrintCommand);
-        }
-
-        [TestMethod]
-        public void CreateErrorCommandShortFlag()
-        {
-            ICommand command = factory.CreateCommand(Constants.HELP_COMMAND_SHORT_FLAG);
-
-            var isPrintCommand = command is HelpCommand;
-
-            Assert.IsTrue(isPrintCommand);
         }
 
 
