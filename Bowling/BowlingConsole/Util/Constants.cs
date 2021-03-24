289ca95d44ea9e51ffe9f2286128f39146d0bc3c
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-24 11:32:59 +0100

    Add constants for PrintCommand

--- Bowling/BowlingConsole/Util/Constants.cs
+++ Bowling/BowlingConsole/Util/Constants.cs
@@ -6,6 +6,10 @@ namespace BowlingConsole.Util
 {
     public static class Constants
     {
+        public const string PRINT_COMMAND_FULL_FLAG = "--print";
+        public const string PRINT_COMMAND_SHORT_FLAG = "-p";
+        public const string PRINT_COMMAND_DESCRIPTION = "Print output in console";
+
 
     }
 }
