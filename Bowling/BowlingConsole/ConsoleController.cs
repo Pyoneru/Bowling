2cfa0af1b1226202741dc85eb4016a651c052951
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-24 15:18:01 +0100

    Create implementation of Controller -> ConsoleController

---
+++ Bowling/BowlingConsole/ConsoleController.cs
@@ -0,0 +1,19 @@
+﻿using BowlingConsole.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingConsole
+{
+    public class ConsoleController : Controller
+    {
+        public ConsoleController(CommandFactory factory, string[] args) : base(factory, args)
+        {
+        }
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
