6837687bd9d26745acd313071d4a1aa15bbdfa93
Pyoneru <piotrkniemczuk@gmail.com>
2021-03-24 12:05:43 +0100

    Add PrintCommand class

---
+++ Bowling/BowlingConsole/Command/PrintCommand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BowlingConsole.Util;
+
+namespace BowlingConsole.Command
+{
+    /// <summary>
+    /// Print output in console.
+    /// </summary>
+    public class PrintCommand : ICommand
+    {
+
+        public string FullFlag { get; set; }
+        public string ShortFlag { get; set; }
+        public string Description { get; set; }
+
+        protected string content;
+
+        public PrintCommand()
+        {
+            FullFlag = Constants.PRINT_COMMAND_FULL_FLAG;
+            ShortFlag = Constants.PRINT_COMMAND_SHORT_FLAG;
+            Description = Constants.PRINT_COMMAND_DESCRIPTION;
+        }
+
+        /// <summary>
+        /// Copy only properties. 
+        /// </summary>
+        /// <returns>Clone of instance</returns>
+        public object Clone()
+        {
+            PrintCommand command = new PrintCommand();
+            command.FullFlag = FullFlag;
+            command.ShortFlag = ShortFlag;
+            command.Description = Description;
+            return command;
+        }
+
+        /// <summary>
+        /// Just print content
+        /// </summary>
+        public void Execute()
+        {
+            Console.WriteLine(content);
+        }
+
+        /// <summary>
+        /// Create content. Sum all objects in data by ToString method.
+        /// if data has not any element. do nothig.
+        /// </summary>
+        /// <param name="data">Data to print in console</param>
+        public void SetData(params object[] data)
+        {
+            if(data.Length >= 1)
+            {
+                content = "";
+                foreach(var obj in data)
+                {
+                    content += obj.ToString() + "\n";
+                }
+            }
+        }
+    }
+}
